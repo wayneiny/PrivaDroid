9a5e59bbfc6edcda06517d626f09fe3ec8124877
Weicheng Cao <weicheng.cao@mail.utoronto.ca>
2019-06-04 15:08:12 -0400

    :sparkles: Connected to Firebase PrivaDroid project. - initialize MobileAd library on start - able to read advertising id from the device

--- app/src/main/java/com/weichengcao/privadroid/MainActivity.java
+++ app/src/main/java/com/weichengcao/privadroid/MainActivity.java
@@ -1,7 +1,9 @@
 package com.weichengcao.privadroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Firebase Mobile Ad [START]
+        MobileAds.initialize(this, "ca-app-pub-1063585474940344~2699806720");
+        // Firebase Mobile Ad [END]
     }
 }
