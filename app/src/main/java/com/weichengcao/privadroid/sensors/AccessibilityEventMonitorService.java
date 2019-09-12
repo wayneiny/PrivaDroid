3ef42d63fbce10370b2527ec91e9b1e43b796e16
Weicheng Cao <weicheng.cao@mail.utoronto.ca>
2019-09-12 14:12:23 -0400

    Minor changes to accessibility service

--- app/src/main/java/com/weichengcao/privadroid/sensors/AccessibilityEventMonitorService.java
+++ app/src/main/java/com/weichengcao/privadroid/sensors/AccessibilityEventMonitorService.java
@@ -1,12 +1,50 @@
 package com.weichengcao.privadroid.sensors;
 
 import android.accessibilityservice.AccessibilityService;
+import android.os.Build;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
 
 public class AccessibilityEventMonitorService extends AccessibilityService {
+
+    private static final String TAG = AccessibilityEventMonitorService.class.getSimpleName();
+
+    private static boolean running = false;
+
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
+        CharSequence packageName = event.getPackageName();
+        int actionType = event.getAction();
+        AccessibilityNodeInfo source = event.getSource();
 
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.M) {
+
+        } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.N) {
+
+        } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.N_MR1) {
+
+        } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.O) {
+
+        } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.O_MR1) {
+
+        } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.P) {
+
+        } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.Q) {
+
+        } else {
+            Log.d(TAG, "Invalid build version = " + Build.VERSION.SDK_INT);
+        }
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    @Override
+    protected void onServiceConnected() {
+        running = true;
+        Log.i(TAG, "Connected to the accessibility service");
     }
 
     @Override
