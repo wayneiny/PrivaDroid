0e5564343517b58ccb4dc53217ec166ba20e7bb7
Weicheng Cao <weicheng.cao@mail.utoronto.ca>
2019-09-18 14:27:22 -0400

    Added activity surveyed and unsurveyed activity top bar

--- app/src/main/java/com/weichengcao/privadroid/ui/SurveyedEventsActivity.java
+++ app/src/main/java/com/weichengcao/privadroid/ui/SurveyedEventsActivity.java
@@ -2,23 +2,30 @@ package com.weichengcao.privadroid.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import androidx.fragment.app.FragmentActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.weichengcao.privadroid.R;
 
 import static com.weichengcao.privadroid.ui.MainScreenActivity.APP_INSTALL_EVENT_TYPE;
 import static com.weichengcao.privadroid.ui.MainScreenActivity.EVENT_TYPE;
 
-public class SurveyedEventsActivity extends FragmentActivity {
+public class SurveyedEventsActivity extends AppCompatActivity {
 
     private int mCurrentEventType;
+    private ImageView mBackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_surveyed_events);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
 
         Intent intent = getIntent();
         if (intent != null) {
@@ -28,5 +35,13 @@ public class SurveyedEventsActivity extends FragmentActivity {
             finish();
             return;
         }
+
+        mBackButton = findViewById(R.id.activity_surveyed_events_back_button);
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
