9658d8218ff59839eed2a8538be8560dd3acb75c
Weicheng Cao <weicheng.cao@mail.utoronto.ca>
2019-09-29 00:22:33 -0400

    Added proactive permission request in Pie

--- app/src/main/java/com/weichengcao/privadroid/util/AndroidSdkConstants.java
+++ app/src/main/java/com/weichengcao/privadroid/util/AndroidSdkConstants.java
@@ -10,6 +10,7 @@ public class AndroidSdkConstants {
     public static final String TEXTVIEW_CLASS_NAME = "android.widget.TextView";
     public static final String BUTTON_CLASS_NAME = "android.widget.Button";
     public static final String SWITCH_CLASS_NAME = "android.widget.Switch";
+    public static final String LISTVIEW_CLASS_NAME = "android.widget.ListView";
 
     /**
      * Packages
